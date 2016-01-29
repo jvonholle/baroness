88987b85a7028493ebc34ba2aa0ec0edbb8415cc
James Von Holle <jrvonhollejr@alaska.edu>
2016-01-28 19:34:04 -0900

    adding jimmy branch

--- board_display/main.cpp
+++ board_display/main.cpp
@@ -53,21 +53,21 @@ int main(int, char const**)
 
     // Set the Icon
     sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
+    if (!icon.loadFromFile("icon.png")) {
         return EXIT_FAILURE;
     }
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
     // Load a sprite to display
     sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
+    if (!texture.loadFromFile( "cute_image.jpg")) {
         return EXIT_FAILURE;
     }
     sf::Sprite sprite(texture);
 
     // Create a graphical text to display
     sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
+    if (!font.loadFromFile( "sansation.ttf")) {
         return EXIT_FAILURE;
     }
     
@@ -76,7 +76,7 @@ int main(int, char const**)
 
     // Load a music to play
     sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
+    if (!music.openFromFile("nice_music.ogg")) {
         return EXIT_FAILURE;
     }
 
