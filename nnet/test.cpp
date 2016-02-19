8350c7afe08b66009d3e529077a2f258bb64539c
James Von Holle <jrvonhollejr@alska.edu>
2016-02-18 16:37:33 -0900

    adding start board to test for easier check on skynet

--- nnet/test.cpp
+++ nnet/test.cpp
@@ -59,6 +59,7 @@ int main()
 	vector<vector<double> > test;
 	string path;
 	vector<string> boardTest;
+	vector<string> startBtest = {"rrrrrrrrrrrr________bbbbbbbbbbbb"};
 	vector<pair<double, string> > rTest;
 	//Fill test vectors
 	//Prompt user for input
@@ -67,8 +68,7 @@ int main()
 	cout << "Path to input file. Blondie24 sized net is included in file blondie in this directory." << endl << ">";
 	cin >> path;
 	test = loadNet(path);
-	//Run and time loop
-	boardTest = boardTestGen(100);
+	//Run and time loop boardTest = boardTestGen(100);
 	auto b = steady_clock::now();		
 	rTest = boardEval(boardTest, test);
 	auto e = steady_clock::now();
@@ -92,12 +92,23 @@ int main()
 	e = steady_clock::now();
 	diff = e - b;
 	cout << "Time 100,000: " << std::chrono::duration<double> (diff).count() << endl;
-	boardTest = boardTestGen(1000000);
-	b = steady_clock::now();		
-	rTest = boardEval(boardTest, test);
-	e = steady_clock::now();
-	diff = e - b;
-	cout << "Time 1,000,000 boards: " << std::chrono::duration<double> (diff).count() << endl;
-		
+//	boardTest = boardTestGen(1000000);
+//	b = steady_clock::now();		
+//	rTest = boardEval(boardTest, test);
+//	e = steady_clock::now();
+//	diff = e - b;
+//	cout << "Time 1,000,000 boards: " << std::chrono::duration<double> (diff).count() << endl;
+	for(int i =0; i<rTest.size(); ++i){
+		if(i%5000==0){
+			cout << "i: "<< i<< " " << rTest[i].first << " " <<  rTest[i].second << endl;
+		}
+}
+
+	
+	rTest = boardEval(startBtest, test);
+	writeNet("out", test);
+	for(int i =0; i<rTest.size(); ++i)
+		cout << "i: "<< i<< " " << rTest[i].first << " " <<  rTest[i].second << endl;
 	return 0;
 }
+
