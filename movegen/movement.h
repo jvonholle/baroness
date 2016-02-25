f032bb3daebd3dd42c38983ac32fe08310bccbd4
Nick Segler <njsegler@alaska.edu>
2016-02-25 00:06:25 -0900

    updating comments on header

--- movegen/movement.h
+++ movegen/movement.h
@@ -3,202 +3,57 @@
 //  checkers
 //
 //
-
-#include "movecheck.h"
-
 #ifndef checkers_movement_h
 #define checkers_movement_h
 
-//check space
-//will return true if pos is empty in cc
-bool cSpace(const string & cs, int pos)
-{
-    if(cs[pos]=='_')
-        return true;
-    return false;
-}
+#include <vector>
+#include <string>
 
-bool jC(const string & jc, int pos, int sel)
-{
-    if(sel==0)
-        if(jc[checkMap(pos).first.first]!=jc[pos]&&cSpace(jc,checkMap(checkMap(pos).first.first).first.first))
-            return true;
-    
-    if(sel==1)
-        if(jc[checkMap(pos).first.second]!=jc[pos]&&cSpace(jc,checkMap(checkMap(pos).first.second).first.second))
-            return true;
-    
-    if(sel==2)
-        if(jc[checkMap(pos).second.second]!=jc[pos]&&cSpace(jc,checkMap(checkMap(pos).second.second).second.second))
-            return true;
-    
-    if(sel==3)
-        if(jc[checkMap(pos).second.first]!=jc[pos]&&cSpace(jc,checkMap(checkMap(pos).second.first).second.first))
-            return true;
-    
-    return false;
-}
 
-//next move
-//gets the next move for the board passed to it
-//uses function cSpace jC and getMove
-string nextMove(string orgi, int p, int sel, bool & keep, int side)
-{
-    if(orgi[p]=='_')
-    {
-        keep=false;
-        return orgi;
-    }
-    if(side!=2)
-    {
-        if(sel==0)
-        {
-            if(cSpace(orgi,checkMap(p).first.first))
-            {
-                orgi[checkMap(p).first.first]='r';
-                
-                orgi[p]='_';
-                keep=true;
-            }else
-            {
-                if(jC(orgi,p,sel))
-                {
-                    orgi[checkMap(checkMap(p).first.first).first.first]=orgi[p];
-                    orgi[checkMap(p).first.first]='_';
-                    orgi[p]='_';
-                    keep=true;
-                    
-                }else
-                {
-                    keep=false;
-                }
-            }
-            return orgi;
-        }
-        
-        if (sel==1)
-        {
-            if(cSpace(orgi,checkMap(p).first.second))
-            {
-                orgi[checkMap(p).first.second]=orgi[p];
-                
-                orgi[p]='_';
-                keep=true;
-            }else
-            {
-                if(jC(orgi,p,sel))
-                {
-                    orgi[checkMap(checkMap(p).first.second).first.second]=orgi[p];
-                    orgi[checkMap(p).first.second]='_';
-                    orgi[p]='_';
-                    keep=true;
-                    
-                }else
-                {
-                    keep=false;
-                }
-            }
-            return orgi;
-        }
-    }
-    if(side!=1)
-    {
-        if (sel==2)
-        {
-            if(cSpace(orgi,checkMap(p).second.second))
-            {
-                orgi[checkMap(p).second.second]=orgi[p];
-                
-                orgi[p]='_';
-                keep=true;
-            }else
-            {
-                if(jC(orgi,p,sel))
-                {
-                    orgi[checkMap(checkMap(p).second.second).second.second]=orgi[p];
-                    orgi[checkMap(p).second.second]='_';
-                    orgi[p]='_';
-                    keep=true;
-                    
-                }else
-                {
-                    keep=false;
-                }
-            }
-            return orgi;
-        }
-        
-        if(sel==3)
-        {
-            if(cSpace(orgi,checkMap(p).second.first))
-            {
-                orgi[checkMap(p).second.first]=orgi[p];
-                
-                orgi[p]='_';
-                
-                keep=true;
-            }else
-            {
-                if(jC(orgi,p,sel))
-                {
-                    orgi[checkMap(checkMap(p).second.first).second.first]=orgi[p];
-                    orgi[checkMap(p).second.first]='_';
-                    orgi[p]='_';
-                    keep=true;
-                    
-                }else
-                {
-                    keep=false;
-                }
-            }
-            return orgi;
-        }
-    }
-    
-    
-    return orgi;
-}
 
-//get boards
-//this generates all next move boards
-//right now it does both sides at once
-//uses functions nextMove
-void getBoards(std::vector<string> & boards, int turn)
-{
-    bool keep=false;
-    int side;
-    string temp;
-    for(int i=0; i<boards[0].length();++i)
-    {
-        
-        if(boards[0][i]=='r')
-        {
-            side=1;
-        }else if(boards[0][i]=='b')
-        {
-            side=2;
-        }
-        else
-        {
-            side=0;
-        }
-        
-        if((turn==0&&(boards[0][i]=='R'||boards[0][i]=='r'))||(turn==1&&(boards[0][i]=='B'||boards[0][i]=='b')))
-            continue;
-        for(int j=0; j<4 ;++j)
-        {
-            
-            
-            temp=nextMove(boards[0],i,j,keep,side);
-            if(keep)
-            {
-                boards.push_back(temp);
-            }
-            keep=false;
-            
-        }
-    }
-}
+//this may be misleading right now
+typedef std::pair<int,std::string> bo; // used for boards with weights
+
+//cSpaceN
+//checks a position on a board to make sure its open
+// takes an int pos: the position to check
+bool cSpaceN(const bo & board, int pos);
+
+//cJumpN
+//takes 2 ints mPos: the position of the current peice
+//  ePos: position of enemy to jump
+//retunrs true if the jump can be made
+bool cJumpN(const bo & board, int mPos, int ePos);
+
+//mBoards
+//takes a bo type board, int pos, int side , int di , and optional bool j
+//the board is the original board to build from
+//int pos is the peice to move
+//int side is whice side is moving
+//      side == 1 peice is moving down the board
+//      side == 0 peice is moving up   the board
+//      side == anything else moves up and down
+//int di is the direction the peice is moving
+//      di == 0 move down left
+//      di == 1 move down right
+//      di == 2 move up right
+//      di == 3 move up left
+//      di must be one of these values
+//bool j is a flag that is set to true if the function call is after a jump
+//
+//retunrs a bo type that contains a board that has a move
+bo mBoards(bo board,int pos, int side, int di ,bool j);
+
+//getBoardsN
+//takes a vector<bo> boards and int turn
+//the vector passed to this function should have the origin board as its first element and no others
+//int turn limits the peices that move
+//      turn == 0 blacks turn
+//      turn == 1 reds turn
+std::vector<bo> getBoardsN(std::vector<bo> boards, int turn);
+
+
+
 
 
 #endif
